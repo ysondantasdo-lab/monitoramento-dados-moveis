3de159de2dfdd467145c77ce0b70ca77808c9691
Yson <ysondantasdo@gmail.com>
2026-08-05 12:15:09 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/mobiledatamonitor/MainActivity.kt
+++ app/src/main/java/com/example/mobiledatamonitor/MainActivity.kt
@@ -320,7 +320,9 @@ class MainActivity : AppCompatActivity() {
     // ---------- Log de falhas via MediaStore (Download/Errodadosmoveis) ----------
         // Adicione as duas funções de classificação AQUI, logo antes do writeLog:
     
-    // --- FUNÇÕES AUXILIARES DE CLASSIFICAÇÃO DE SINAL ---
+        // --- FUNÇÕES AUXILIARES DE CLASSIFICAÇÃO DE SINAL ---
+
+        // --- FUNÇÕES AUXILIARES DE CLASSIFICAÇÃO DE SINAL ---
 
     // Função auxiliar para classificar a força do sinal (dBm)
     private fun getSignalStrengthLabel(dbm: Int?): String {
@@ -388,4 +390,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
